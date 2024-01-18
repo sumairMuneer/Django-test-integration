b6e3f372e83a5b8605f708f8560bab2459c57bea
sumairMuneer <53559851+sumairMuneer@users.noreply.github.com>
2024-01-18 18:25:32 +0500

    testing

--- tests.py
+++ tests.py
@@ -12,6 +12,7 @@ class QuestionModelTests1(TestCase):
         """
         # a=2
         self.assertGreaterEqual(2,2)
+        
 
     def test_was_published_recently_with_future_question1(self):
         """
